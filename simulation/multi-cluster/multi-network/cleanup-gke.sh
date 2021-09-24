1bbdef4da4ead6e0a503fd8a1dd87d50da40ea32
yerinu2019 <yerinu2019@gmail.com>
2021-09-24 15:28:07 +0000

    refactor

--- simulation/multi-cluster/multi-network/cleanup-gke.sh
+++ simulation/multi-cluster/multi-network/cleanup-gke.sh
@@ -1,11 +1,11 @@
 echo "Deleting client cluster..."
-gcloud container clusters delete client --zone us-west2-a -y
+gcloud container clusters delete client --zone us-west2-a -q
 
 echo "Deleting api cluster..."
-gcloud container clusters delete api --zone us-central1-a -y
+gcloud container clusters delete api --zone us-central1-a -q
 
 echo "Deleting client network..."
-gcloud compute networks delete vpc-client -y
+gcloud compute networks delete vpc-client -q
 
 echo "Deleting api network..."
-gcloud compute networks delete vpc-api -y
+gcloud compute networks delete vpc-api -q
