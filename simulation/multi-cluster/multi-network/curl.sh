7030418bafadac5e003c3a436c4d22c1f7df49b5
yerinu2019 <yerinu2019@gmail.com>
2021-09-24 00:48:15 +0000

    curl caller

--- simulation/multi-cluster/multi-network/curl.sh
+++ simulation/multi-cluster/multi-network/curl.sh
@@ -3,4 +3,5 @@
 # S3: caller app label
 # $5: called api service
 POD=`kubectl --context $1 -n $2 get pod -l app=$3 -o jsonpath='{.items..metadata.name}'`
-kubectl --context $1 -n $2 $POD -- curl $4
+echo $POD
+kubectl --context $1 -n $2 get pod $POD -- curl $4
