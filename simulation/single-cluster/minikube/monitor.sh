315bc3d803301d24f83ec167b9cb6ed78861f864
yerinu2019 <yerinu2019@gmail.com>
2021-09-25 01:38:24 +0000

    minikube

--- simulation/single-cluster/minikube/monitor.sh
+++ simulation/single-cluster/minikube/monitor.sh
@@ -1,9 +1,9 @@
-kubectl --context client apply -f ${HOME}/istio-${ISTIO_VERSION}/samples/addons/prometheus.yaml
-kubectl --context client apply -f ${HOME}/istio-${ISTIO_VERSION}/samples/addons/kiali.yaml
+kubectl apply -f ${HOME}/istio-${ISTIO_VERSION}/samples/addons/prometheus.yaml
+kubectl apply -f ${HOME}/istio-${ISTIO_VERSION}/samples/addons/kiali.yaml
 
-kubectl --context client get pods -n istio-system
+kubectl get pods -n istio-system
 
-kubectl --context client port-forward svc/kiali 8080:20001 -n istio-system >> /dev/null &
+kubectl port-forward svc/kiali 8080:20001 -n istio-system >> /dev/null &
 
 echo "Open the Kiali web interface on the client cluster. In Cloud Shell, select Web Preview, and then select Preview on port 8080."
 echo "From the left menu, select Graph."
