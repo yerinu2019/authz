b36ab1352cb0cca31e2b0fea63374c16d068ddb6
yerinu2019 <yerinu2019@gmail.com>
2021-09-25 00:04:22 +0000

    minikube

--- simulation/single-cluster/minikube/install-minikube-mac.sh
+++ simulation/single-cluster/minikube/install-minikube-mac.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Install Mac minikube"
 cd $HOME
-brew minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-darwin-amd64
+sudo install minikube-darwin-amd64 /usr/local/bin/minikube
 cd $SCRIPT_DIR
