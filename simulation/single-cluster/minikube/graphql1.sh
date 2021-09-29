75ad0c03efc5ad8074c5057d1c204fd455473d18
yerinu2019 <yerinu2019@gmail.com>
2021-09-29 02:39:27 +0000

    graphql test

---
+++ simulation/single-cluster/minikube/graphql1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+POD=`kubectl -n $1 get pod -l app=$2 -o jsonpath='{.items..metadata.name}'`
+echo $POD
+kubectl -n $1 exec $POD -- curl -g -sS \
+-X POST \
+-H "Content-Type: application/json" \
+-d '{"query":"query{showCollection {items {title firstEpisodeDate lastEpisodeDate henshinMp4 { url }}}}"}' \
+$3
