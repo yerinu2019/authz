049607d526cfab11f85b236b3fddd8afa11a359b
owner1 <seyoungsong2017@gmail.com>
2021-11-20 16:52:49 -0500

    change bundle name

--- simulation/single-cluster/minikube/policy/rego/bundle.sh
+++ simulation/single-cluster/minikube/policy/rego/bundle.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "build"
-opa build -b -o bundle$1.tar.gz .
+opa build -b -o authz-bundle.tar.gz .
 echo "upload"
-gsutil cp -p bundle$1.tar.gz gs://test-opa-policy-bundles
+gsutil cp -p authz-bundle.tar.gz gs://test-opa-policy-bundles
